0af36564e73409047b9e58b532d5657d58899550
yusuken1121 <yusuken1121@gmail.com>
2024-12-04 21:07:38 -0800

    💄 Add styles in index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,58 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const Index = () => {
   return (
-    <View>
-      <Text>Hello world</Text>
+    <View style={styles.container}>
+      {/* Header */}
+      <View>
+        <Text>Memo</Text>
+        <Text>ログアウト</Text>
+      </View>
+
+      {/* Content */}
+      <View>
+        <View>
+          <Text>Shopping List</Text>
+          <Text>2024/12/3</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+      </View>
+
+      <View>
+        <View>
+          <Text>Shopping List</Text>
+          <Text>2024/12/3</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+      </View>
+
+      <View>
+        <View>
+          <Text>Shopping List</Text>
+          <Text>2024/12/3</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+      </View>
+
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
 export default Index;
