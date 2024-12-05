f15f2d0945ec393614a3ca2684abeaec6a5b4060
yusuken1121 <yusuken1121@gmail.com>
2024-12-04 21:29:02 -0800

    💄 Add Header Style

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,9 +4,11 @@ const Index = () => {
   return (
     <View style={styles.container}>
       {/* Header */}
-      <View>
-        <Text>Memo</Text>
-        <Text>ログアウト</Text>
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>Memo</Text>
+          <Text style={styles.headerRight}>ログアウト</Text>
+        </View>
       </View>
 
       {/* Content */}
@@ -50,8 +52,36 @@ const Index = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#FFFFFF",
+  },
+  header: {
+    backgroundColor: "#FFFFFF",
+    height: 104,
+    justifyContent: "flex-end",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.1, // 影の透明度 (0〜1)
+    shadowRadius: 4, // 影のぼかし具合
+    // Shadow for Android
+    elevation: 5,
+  },
+  headerInner: {
     alignItems: "center",
+  },
+  headerRight: {
+    position: "absolute",
+    right: 16,
+    bottom: 16,
+    color: "rgba(0,0,0,0.7)",
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: "regular",
   },
 });
 
